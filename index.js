58721e022b66258f70bad712f66755f79e22c469
Dev Singh <dsingh@imsa.edu>
2021-01-19 10:27:53 -0600

    fix routing issues

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016 Red Hat Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 const Keycloak = require('keycloak-connect');
 const hogan = require('hogan-express');
 const express = require('express');
@@ -51,14 +35,14 @@ var keycloak = new Keycloak({
 
 async function getUserInfo(bearer_token) {
   const myHeaders = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${bearer_token}`
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${bearer_token}`
   };
-  
+
   const response = await fetch('https://webauth.epochml.org/auth/realms/epochml.org/protocol/openid-connect/userinfo', {
     method: 'GET',
     headers: myHeaders,
-  }).catch(error => {console.error(error);});
+  }).catch(error => { console.error(error); });
   if (response.status === 200) {
     return response.json();
   } else {
@@ -67,110 +51,107 @@ async function getUserInfo(bearer_token) {
 }
 
 app.use(keycloak.middleware());
-app.get('/', keycloak.protect(), async function (req, res) {
-  let email;
-  let name;
-  try {
-    const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
-    let userInfo = await getUserInfo(bearer_token);
-    email = userInfo.email;
-    name = userInfo.name
-  } catch (e) {
-    res.json({
-      message: "Could not get user authorization information.",
-      error: e
-    })
-    return
-  }
-  res.render('index.html', {email, name})
-  return
-})
-
-app.post('/addURL', keycloak.protect(), async function (req, res) {
-  let email;
-  try {
-    const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
-    let userInfo = await getUserInfo(bearer_token);
-    email = userInfo.email;
-  } catch (e) {
-    res.status(500).json({
-      message: "Could not get user authorization information.",
-      error: e
-    })
-    return
-  }
-  const db = await csvdb("links.csv", ["url", "name", "email"]).catch((e) => {
-    console.error(e)
-    res.status(500).json({
-      message: "Error getting information from DB"
-    })
-    return
-  });
-  const url = req.query.url;
-  const name = req.query.name; 
-  if (url === undefined || name === undefined) {
-    res.status(400).json({
-      message: "Either url or name was not provided."
-    })  
-    return
-  }
-  let old;
-  try {
-    old = await db.get({name});
-  } catch {
-    res.status(500).json({
-      message: "Error getting information from DB"
-    })
-    return
-  }
-  
-  if (old.length > 0) {
-    res.status(400).json({
-      message: "This short URL has already been taken. Please try another."
-    });
-  } else {
-    await db.add({url, name, email});
-    res.json({
-      url: req.query.url,
-      shortURL: `https://out.epochml.org/${req.query.name}`,
-      email
-    });
-    return
-  }
-
-});
-
-app.get('/getUserURLs', keycloak.protect(), async function (req, res) {
-  let email;
-  try {
-    const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
-    let userInfo = await getUserInfo(bearer_token);
-    email = userInfo.email;
-  } catch (e) {
-    res.json({
-      message: "Could not get user authorization information.",
-      error: e
-    })
-  }
-  const db = await csvdb("links.csv", ["url", "name", "email"]).catch((e) => {
-    console.error(e)
-    res.status(500).json({
-      message: "Error getting information from DB"
-    })
-    return
-  });
-  const all = await db.get({email});
-  res.json(all)
-})
-
-app.get('/:id', async function(req,res) {
+app.get('/:id', async function (req, res) {
   const name = req.params.id;
+  if (name === "admin") {
+    let email;
+    let name;
+    try {
+      const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
+      let userInfo = await getUserInfo(bearer_token);
+      email = userInfo.email;
+      name = userInfo.name
+    } catch (e) {
+      res.json({
+        message: "Could not get user authorization information.",
+        error: e
+      })
+      return
+    }
+    res.render('index.html', { email, name })
+    return
+  } else if (name === "getUserURLs") {
+    let email;
+    try {
+      const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
+      let userInfo = await getUserInfo(bearer_token);
+      email = userInfo.email;
+    } catch (e) {
+      res.json({
+        message: "Could not get user authorization information.",
+        error: e
+      })
+    }
+    const db = await csvdb("links.csv", ["url", "name", "email"]).catch((e) => {
+      console.error(e)
+      res.status(500).json({
+        message: "Error getting information from DB"
+      })
+      return
+    });
+    const all = await db.get({ email });
+    res.json(all)
+  }
   const db = await csvdb("links.csv", ["url", "name", "email"]);
-  const url = await db.get({name});
+  const url = await db.get({ name });
   try {
     res.redirect(url[0].url);
   } catch {
-    
+
   }
-  
+
+  app.post('/addURL', keycloak.protect(), async function (req, res) {
+    let email;
+    try {
+      const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
+      let userInfo = await getUserInfo(bearer_token);
+      email = userInfo.email;
+    } catch (e) {
+      res.status(500).json({
+        message: "Could not get user authorization information.",
+        error: e
+      })
+      return
+    }
+    const db = await csvdb("links.csv", ["url", "name", "email"]).catch((e) => {
+      console.error(e)
+      res.status(500).json({
+        message: "Error getting information from DB"
+      })
+      return
+    });
+    const url = req.query.url;
+    const name = req.query.name;
+    if (url === undefined || name === undefined) {
+      res.status(400).json({
+        message: "Either url or name was not provided."
+      })
+      return
+    }
+    let old;
+    try {
+      old = await db.get({ name });
+    } catch {
+      res.status(500).json({
+        message: "Error getting information from DB"
+      })
+      return
+    }
+
+    if (old.length > 0) {
+      res.status(400).json({
+        message: "This short URL has already been taken. Please try another."
+      });
+    } else {
+      await db.add({ url, name, email });
+      res.json({
+        url: req.query.url,
+        shortURL: `https://out.epochml.org/${req.query.name}`,
+        email
+      });
+      return
+    }
+
+  });
+
 })
