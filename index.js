4263eacfe96fa7480d4b05238bd5b151d4a7362c
Dev Singh <dsingh@imsa.edu>
2021-01-19 09:33:01 -0600

    push new routes

--- index.js
+++ index.js
@@ -14,11 +14,12 @@
  * the License.
  */
 
-var Keycloak = require('keycloak-connect');
-var hogan = require('hogan-express');
-var express = require('express');
-var session = require('express-session');
-var csvdb = require("csv-database");
+const Keycloak = require('keycloak-connect');
+const hogan = require('hogan-express');
+const express = require('express');
+const session = require('express-session');
+const csvdb = require("csv-database");
+const fetch = require('node-fetch');
 
 var app = express();
 var server = app.listen(9215, function () {
@@ -44,47 +45,84 @@ app.use(session({
   store: memoryStore
 }));
 
-// Provide the session store to the Keycloak so that sessions
-// can be invalidated from the Keycloak console callback.
-//
-// Additional configuration is read from keycloak.json file
-// installed from the Keycloak web console.
-
 var keycloak = new Keycloak({
   store: memoryStore
 });
 
-// Install the Keycloak middleware.
-//
-// Specifies that the user-accessible application URL to
-// logout should be mounted at /logout
-//
-// Specifies that Keycloak console callbacks should target the
-// root URL.  Various permutations, such as /k_logout will ultimately
-// be appended to the admin URL.
+async function getUserInfo(bearer_token) {
+  const myHeaders = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${bearer_token}`
+  };
+  
+  return fetch('https://webauth.epochml.org/auth/realms/epochml.org/protocol/openid-connect/userinfo', {
+    method: 'GET',
+    headers: myHeaders,
+  })
+  .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong on api server!');
+      }
+    })
+    .then(response => {
+      console.debug(response);
+    }).catch(error => {
+      console.error(error);
+    });
+}
 
 app.use(keycloak.middleware());
 app.get('/:id', async function(req,res) {
   const name = req.params.id;
-  const db = await csvdb("links.csv", ["name","url"]);
-  const url = await db.get({name})
-  res.redirect(url[0].url)
+  const db = await csvdb("links.csv", ["name","url", "user"]);
+  const url = await db.get({name});
+  res.redirect(url[0].url);
 })
 app.get('/', keycloak.protect(), async function (req, res) {
-  const db = await csvdb("links.csv", ["name","url"]);
+  let email;
+  try {
+    const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
+    const userInfo = await getUserInfo(bearer_token);
+    email = userInfo.email;
+  } catch (e) {
+    res.json({
+      message: "Could not get user authorization information.",
+      error: e
+    })
+  }
+  const db = await csvdb("links.csv", ["name","url", "user"]);
   const url = req.query.url;
   const name = req.query.name; 
-  await db.add({url, name})
-  res.json({
-    url: req.query.url,
-    name: req.query.name
-  })
+  const old = await db.get({name});
+  if (old.length > 0) {
+    res.json({
+      message: "This short URL has already been taken. Please try another."
+    });
+  } else {
+    await db.add({url, name, email});
+    res.json({
+      url: req.query.url,
+      shortURL: `https://out.epochml.org/${req.query.name}`,
+      email
+    });
+  }
+
 });
 
-
-// app.get('/protected/resource', keycloak.protect(), function (req, res) {
-//   res.render('index', {
-//     result: JSON.stringify(JSON.parse(req.session['keycloak-token']), null, 4),
-//     event: '1. Access granted to Default Resource\n'
-//   });
-// });
+app.get('/getUserURLs', keycloak.protect(), async function (req, res) {
+  let email;
+  try {
+    const bearer_token = JSON.parse(req.session['keycloak-token']).access_token;
+    const userInfo = await getUserInfo(bearer_token);
+    email = userInfo.email;
+  } catch (e) {
+    res.json({
+      message: "Could not get user authorization information.",
+      error: e
+    })
+  }
+  const all = await db.get({email});
+  res.json(all)
+})
