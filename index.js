218f0efbb6542ac698c9c974a72082a54a90a06c
Dev Singh <dsingh@imsa.edu>
2021-01-23 21:40:04 -0600

    use sqlite

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const Keycloak = require('keycloak-connect');
 const hogan = require('hogan-express');
 const express = require('express');
 const session = require('express-session');
-const csvdb = require("csv-database");
 const fetch = require('node-fetch');
 const baseURL = process.env.baseURL || 'out.epochml.org';
 const favicon = require('serve-favicon');
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database('./db.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+})
 
 var app = express();
-var server = app.listen(9200, function () {
+var server = app.listen(9215, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
@@ -54,6 +61,77 @@ async function getUserInfo(bearer_token) {
   }
 }
 
+async function addURLToDB(name, url, email) {
+  return new Promise(function(resolve, reject) {
+    db.serialize(function() {
+      const stmt = db.prepare("INSERT INTO urlData (name, url, email) VALUES (?, ?, ?)");
+      stmt.run([name, url, email], function(err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({name, url, email})
+        }
+      })
+    })
+  })
+}
+async function getDataForEmail(email) {
+  return new Promise(function(resolve, reject) {
+    db.serialize(function() {
+      const stmt = db.prepare("SELECT * FROM urlData WHERE email=?");
+      stmt.all([email], function(err, data) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  })
+}
+async function removeURLfromDB(name) {
+  return new Promise(function(resolve, reject) {
+    db.serialize(function() {
+      const stmt = db.prepare("DELETE FROM urlData WHERE name=?");
+      stmt.run([name], function(err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(name)
+        }
+      })
+    })
+  })
+}
+async function getRedirectURL(name) {
+  return new Promise(function(resolve, reject) {
+    db.serialize(function() {
+      const stmt = db.prepare("SELECT url FROM urlData WHERE name=?");
+      stmt.all([name], function(err, data) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  })
+}
+async function updateRecord(name, url) {
+  return new Promise(function(resolve, reject) {
+    db.serialize(function() {
+      const stmt = db.prepare("UPDATE urlData SET url=?, name=? WHERE name=?");
+      stmt.run([url, name, name], function(err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({name, url})
+        }
+      })
+    })
+  })
+}
+
 app.use(keycloak.middleware());
 app.get('/', keycloak.protect(), async function (req, res) {
   let email;
@@ -84,43 +162,39 @@ app.post('/addURL', keycloak.protect(), async function (req, res) {
     })
     return
   }
-  const db = await csvdb("links.csv", ["url", "name", "email"]).catch((e) => {
-    res.status(500).json({
-      message: "Error getting information from DB"
-    })
-    return
-  });
   const url = req.query.url;
   const name = req.query.name;
+  if (url.indexOf(baseURL) > -1 ) {
+    res.json({
+      message: `The origin URL cannot be a path of ${baseURL}`
+    })
+    return
+  }
   if (url === undefined || name === undefined) {
     res.status(400).json({
       message: "Either url or name was not provided."
     })
     return
   }
-  let old;
-  try {
-    old = await db.get({ name });
-  } catch {
-    res.status(500).json({
-      message: "Error getting information from DB"
-    })
-    return
-  }
-
-  if (old.length > 0) {
-    res.status(400).json({
-      message: "This short URL has already been taken. Please try another."
-    });
-  } else {
-    await db.add({ url, name, email });
+  addURLToDB(name, url, email).then((obj) => {
     res.json({
-      url: req.query.url,
-      shortURL: `https://out.epochml.org/${req.query.name}`,
-      email
+      url: obj.url,
+      shortURL: `https://out.epochml.org/${obj.name}`,
+      email: obj.email
     });
-    return
-  }
+  }).catch((err) => {
+    if (err.errno == 19) {
+      res.status(409).json({
+        message: "This short URL has already been taken. Please try another."
+      })
+    } else {
+      res.status(500).json({
+        message: "The short URL could not be added. Please try again."
+      })
+    }
+
+  })
+  return
 
 });
 
@@ -135,16 +209,9 @@ app.get('/mylinks', keycloak.protect(), async function (req, res) {
   } catch (e) {
     res.status(500).render('500')
   }
-  const db = await csvdb("links.csv", ["url", "name", "email"]).catch((e) => {
-    console.error(e)
-    res.status(500).json({
-      message: "Error getting information from DB"
-    })
-    return
-  });
-  const all = await db.get({ email });
+  const data = await getDataForEmail(email).catch(() => {res.status(500).render('500'); return});
   res.render('mylinks', {
-    data: all,
+    data,
     name,
     email,
     baseURL
@@ -153,50 +220,43 @@ app.get('/mylinks', keycloak.protect(), async function (req, res) {
 
 app.delete('/deleteLink', keycloak.protect(), async function (req, res) {
   const name = req.query.name;
-  const db = await csvdb("links.csv", ["url", "name", "email"]);
-  try {
-    await db.delete({ name })
-  } catch (e) {
+  removeURLfromDB(name).then(() => {
+    res.json({
+      name, deleted: true
+    })
+    return
+  }).catch(() => {
     res.status(500).json({
       message: "Could not delete the link. Please try again."
     })
     return
-  }
-  res.json({
-    name,
-    deleted: true
   })
-
 })
 app.put('/updateLink', keycloak.protect(), async function (req, res) {
   const name = req.query.name;
   const url = req.query.url;
-  let dbCheck;
-  const db = await csvdb("links.csv", ["url", "name", "email"]);
-  try {
-    await db.edit({ name }, { url })
-    dbCheck = await db.get({ name });
-  } catch (e) {
+  if (url.indexOf(baseURL) > -1 ) {
+    res.json({
+      message: `The origin URL cannot be a path of ${baseURL}`
+    })
+    return
+  }
+  updateRecord(name, url).then((data) => {
+    res.json(data);
+    return;
+  }).catch(() => {
     res.status(500).json({
       message: "Could not update the link. Please try again."
     })
     return
-  }
-  res.json(dbCheck[0])
-
+  })
 })
 app.get('/:id', async function (req, res) {
   const name = req.params.id;
-  const db = await csvdb("links.csv", ["url", "name", "email"]);
-  const url = await db.get({ name });
-  try {
-    if (url.length < 1) {
-      res.status(404).render('404.html')
-    } else {
-      res.redirect(url[0].url);
-    }
-  } catch {
-    res.status(500).render('500')
-  }
-
+  const ts = Date.now();
+  getRedirectURL(name).then(url => {
+    res.redirect(url[0].url);
+    return;
+  }).catch(res.status(500).render('500'))
+  console.log(ts, name);
 })
